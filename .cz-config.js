aac14b7f01a93c68b7f27b17c0be3de44ae07247
guoshicheng <programmerguo@gmail.com>
2022-02-15 20:15:56 +0800

    feat: feat: feat: test

--- .cz-config.js
+++ .cz-config.js
@@ -16,12 +16,12 @@ module.exports = {
     { value: 'revert', name: 'revert:      回退' },
     { value: 'build', name: 'build:      打包 ' }
   ],
-  message: {
-    type: '请选择提交类型',
-    customScope: '请输入修改范围(可选)',
-    subject: '请简要描述提交(必填)',
+  messages: {
+    type: '请选择提交类型:',
+    customScope: '请输入修改范围(可选):',
+    subject: '请简要描述提交(必填):',
     body: '请输入详细描述(可选):',
-    footer: '请输入要关闭的issue(可选)',
+    footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交?(y/n/e/h)'
   },
   skipQuestions: ['body', 'footer'],
